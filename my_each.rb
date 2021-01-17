99bb9f0c4ea592d39a7bb77c7c152373b4378076
Julia Kim <juliaykim6@gmail.com>
2021-01-17 01:43:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,10 @@
 def my_each(array)
   # put argument(s) here
   # code here
-  if block_given?
-  i = 0
-  while i < array.length
-    yield (word[i])
-  end
-  array
-else  ""
-end
+  while array.length > 0 
+    yield (array[i])
+    i = i + 1
+  end 
+  array 
+end 
 end
