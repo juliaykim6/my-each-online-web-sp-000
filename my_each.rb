47353f7b0852c2013515dd90eebbab675675bec0
Julia Kim <juliaykim6@gmail.com>
2021-01-17 01:43:06 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
 def my_each(array)
   # put argument(s) here
   # code here
-  while array.length > 0 
+  while array.length > 0
     yield (array[i])
     i = i + 1
-  end 
-  array 
-end 
+  end
+  array
+end
 end
